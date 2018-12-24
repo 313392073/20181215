8e53275d307ea5c684979d000c23a3245f5c37d7
313392073 <313392073@qq.com>
2018-12-24 17:12:59 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -17,27 +17,32 @@ import SelectId from '@/pages/SelectId'//注册
 import RegPage from '@/pages/RegPage'//注册
 import Login from '@/pages/Login'//登录
 import FindPwd from '@/pages/FindPwd'//找回密码
+import PracticReport from '@/pages/PracticReport'// 在线测试学生练习结果(学生)
+import AssignGroup from '@/pages/AssignGroup'    // 分配小组
+import AssignGroupCase from '@/pages/AssignGroupCase'    // 小组分配情况 （制作的入口）
+import FindPyramid from '@/pages/FindPyramid'//寻找棱锥
+import MakePyramid from '@/pages/MakePyramid'//制作棱锥
+import TaskDetail from '@/pages/TaskDetail'// 任务详情
+
+import VideoShare from '@/pages/VideoShare'//视频分享
 import SideAreaFormula from '@/pages/SideAreaFormula'//正棱锥侧面积
 import RegularAreaFormula from '@/pages/RegularAreaFormula'//正棱锥表面积
 import VideDetail from '@/pages/VideDetail'//视频详情
-import TaskDetail from '@/pages/TaskDetail'// 任务详情
+
+import TeaHomeworkShare from '@/pages/TeaHomeworkShare'    // 老师端作业分享1
 import LineRelation from '@/pages/LineRelation'//线线关系
 import AftClaExp from '@/pages/AftClaExp'//课后实验
-import VideoShare from '@/pages/VideoShare'//视频分享
-import FindPyramid from '@/pages/FindPyramid'//寻找棱锥
-import MakePyramid from '@/pages/MakePyramid'//制作棱锥
+
+
 import HomeworkProblems from '@/pages/HomeworkProblems'//课后习题
 import CourseManagement from '@/pages/CourseManagement'//课件管理
 import PraafterClass from '@/pages/PraafterClass'//课后练习
 import ClassLearning from '@/pages/ClassLearning'//课堂学习
 import LearningReport from '@/pages/LearningReport'//课堂学习成绩报告(学生) 
 import TestReport from '@/pages/TestReport'//课堂学习成绩报告(学生) 
-import PracticReport from '@/pages/PracticReport'// 在线测试学生练习结果(学生)
 import JobResults from '@/pages/JobResults'   // 作业结果(学生) 
 import ClassTotalResult from '@/pages/ClassTotalResult'    // 课堂学习测试结果统计报告
-import AssignGroup from '@/pages/AssignGroup'    // 分配小组
-import AssignGroupCase from '@/pages/AssignGroupCase'    // 小组分配情况
-import TeaHomeworkShare from '@/pages/TeaHomeworkShare'    // 老师端作业分享1
+
 import TeaShareDetail from '@/pages/TeaShareDetail'    // 老师端作业分享2
 import ExpregPyramid from '@/pages/ExpregPyramid'    // 探索正三棱锥
 import CalSurfaceArea from '@/pages/CalSurfaceArea'    // 计算表面积
